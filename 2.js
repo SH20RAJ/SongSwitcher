79eb1f2512b505b1588309de72bb01181db0e204
Sh Raj <shaswatraj3@gmail.com>
2024-03-21 11:10:24 +0530

    Update 2.js

--- 2.js
+++ 2.js
@@ -36,7 +36,7 @@ function fetchSongs(query,random,url) {
         .then(data => {
                   console.log("out",songs,data);
             
-            if (data.status === 'SUCCESS' && data.data && data.data.results) {
+            if ( data.data && data.data.results) {
               if(random){
                 songs = shuffleArray([...songs, ...data.data.results]);
                   console.log("in",songs,data);
